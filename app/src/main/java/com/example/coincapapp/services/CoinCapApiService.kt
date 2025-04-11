82edce2033933a9fd5505eb69b23c31e4cf83e43
Henry <henry.2154@hotmail.com>
2025-04-10 21:49:30 -0500

    ktor implementatio and bottonnavigation

--- app/src/main/java/com/example/coincapapp/services/CoinCapApiService.kt
+++ app/src/main/java/com/example/coincapapp/services/CoinCapApiService.kt
@@ -12,7 +12,7 @@ class CoinCapApiService @Inject constructor(
     private val client : HttpClient
 ){
     suspend fun getAssets(): AssetsResponse{
-        val response: HttpResponse = client.get(urlString = "https://c8dc6bef-802f-4ebc-b955-7b693182aa53.mock.pstmn.io/v3/assets?apiKey=d5d5e048dd835a78d3cf1cb408fb715468d60ab8c7bd4e99b6bfd2ab816015f5")
+        val response: HttpResponse = client.get(urlString = "uri")
         return  response.body()
     }
 }
