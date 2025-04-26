fe21eec33b8a94c478f686e0934744f30632b1f4
Henry <henry.2154@hotmail.com>
2025-04-25 23:31:55 -0500

    login with firebase

--- app/src/main/java/com/example/coincapapp/views/SettingsView.kt
+++ app/src/main/java/com/example/coincapapp/views/SettingsView.kt
@@ -1,14 +1,82 @@
 package com.example.coincapapp.views
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.coincapapp.viewModels.LoginViewModel
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-fun Settings (){
-    Box(modifier = Modifier.fillMaxSize()){
-        Text("Settings")
+fun Settings (viewModel: LoginViewModel = viewModel()){
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val message by viewModel.message.collectAsState()
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Iniciar Sesión")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Contraseña") },
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { viewModel.login(email, password) },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Iniciar Sesión")
+        }
+
+        if (message.isNotEmpty()) {
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = message)
+        }
     }
+}
+
+@Preview(
+    showBackground = true
+)
+@Composable
+fun SettingsPreview() {
+    Settings()
 }
